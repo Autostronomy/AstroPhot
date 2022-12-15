227e61c1659781465d188b4caedba6241a8e052f
Connor Stone <connorstone628@gmail.com>
2022-12-15 16:09:30 -0500

    distribution package built for 0.1.0

---
+++ build/lib/autoprof/__init__.py
@@ -0,0 +1,26 @@
+from . import models
+from . import image
+from . import plots
+from . import utils
+from . import fit
+
+# meta data
+__version__ = "0.1.0"
+__author__ = "Connor Stone"
+__email__ = "connorston628@gmail.com"
+
+def run_from_terminal():
+    import sys
+    print("running from the terminal, not sure if it will catch me.")
+    if any("tutorial" in A for A in sys.argv):
+        import requests
+        tutorials = [
+            "https://raw.github.com/ConnorStoneAstro/AutoProf-2/main/docs/tutorials/GettingStarted.ipynb",
+            "https://raw.github.com/ConnorStoneAstro/AutoProf-2/main/docs/tutorials/CombinedModels.ipynb",
+            "https://raw.github.com/ConnorStoneAstro/AutoProf-2/main/docs/tutorials/ModelZoo.ipynb",
+        ]
+        for url in tutorials:
+            R = requests.get(url)
+            with open(url[url.rfind("/")+1:], "w") as f:
+                f.write(R.text)
+        print("collected the tutorials")
