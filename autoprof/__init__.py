c4b56a54b58ec95b8e3d4eb2da9865520239acd0
Connor Stone <connorstone628@gmail.com>
2023-01-21 21:56:42 +0400

    Adding configuration file capability, still in development

--- autoprof/__init__.py
+++ autoprof/__init__.py
@@ -12,8 +12,25 @@ __email__ = "connorston628@gmail.com"
 
 def run_from_terminal():
     import sys
+    import argparse
     AP_config.ap_logger.debug("running from the terminal, not sure if it will catch me.")
-    if any("tutorial" in A for A in sys.argv):
+    parser = argparse.ArgumentParser(
+        prog = "autoprof",
+        description = "Fast and flexible photometry reduction package",
+        epilog = "Please contact connor stone (connorstone628@gmail.com) for further assistance."
+    )
+
+    parser.add_argument("filename", type = str, help = "the path to the configuration file. Or just 'tutorial' to download tutorials.")
+    parser.add_argument(
+        "-c", "--config",
+        type = str,
+        default = "autoprof",
+        choices = ["autoprof", "galfit"],
+        help = "The type of configuration file being being provided. One of: autoprof, galfit.",
+    )
+    args = parser.parse_args()
+    
+    if args.filename in ["tutorial", "tutorials"]:
         import requests
         tutorials = [
             "https://raw.github.com/ConnorStoneAstro/AutoProf-2/main/docs/tutorials/GettingStarted.ipynb",
@@ -31,3 +48,11 @@ def run_from_terminal():
                 print(f"WARNING: couldn't find tutorial: {url[url.rfind('/')+1:]} check internet conection")
                     
         AP_config.ap_logger.info("collected the tutorials")
+    elif args.config == "autoprof":
+        from .parse_config import basic_config
+        basic_config(args.filename)
+    elif args.config == "galfit":
+        from .parse_config import galfit_config
+        galfit_config(args.filename)
+    else:
+        raise ValueError(f"Unrecognized configuration file format {args.config}. Should be one of: autoprof, galfit")
---
+++ autoprof/parse_config/galfit_config.py
@@ -0,0 +1,9 @@
+import numpy
+from astropy.io import fits
+from ..image import Target_Image
+from ..models import AutoProf_Model
+from ..fit import LM
+
+__all__ = ["galfit_config"]
+def galfit_config(config_file):
+    raise NotImplementedError("GALFIT configuration file reader not yet implemeneted, it will be available soon!")
