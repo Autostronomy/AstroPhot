f3327978e4617d3e6d1a88f933d1abe558196fa4
Connor Stone <connorstone628@gmail.com>
2023-02-02 17:03:27 -0500

    command line interface options added

--- autoprof/__init__.py
+++ autoprof/__init__.py
@@ -16,20 +16,21 @@ def run_from_terminal():
     AP_config.ap_logger.debug("running from the terminal, not sure if it will catch me.")
     parser = argparse.ArgumentParser(
         prog = "autoprof",
-        description = "Fast and flexible astronomical image photometry package",
-        epilog = "Please contact connor stone (connorstone628@gmail.com) for further assistance."
+        description = "Fast and flexible astronomical image photometry package. For the documentation go to: https://github.com/ConnorStoneAstro/AutoProf-2",
+        epilog = "Please see the documentation or contact connor stone (connorstone628@gmail.com) for further assistance."
     )
-
     parser.add_argument(
         "filename",
         nargs = "?",
+        metavar = "configfile",
         help = "the path to the configuration file. Or just 'tutorial' to download tutorials."
     )
     parser.add_argument(
-        "-c", "--config",
+        "--config",
         type = str,
         default = "autoprof",
         choices = ["autoprof", "galfit"],
+        metavar = "format",
         help = "The type of configuration file being being provided. One of: autoprof, galfit.",
     )
     parser.add_argument(
@@ -38,8 +39,44 @@ def run_from_terminal():
         version = f"%(prog)s {__version__}",
         help = "print the current AutoProf version to screen",
     )
+    parser.add_argument(
+        "--log",
+        type = str,
+        metavar = "logfile.log",
+        help = "set the log file name for AutoProf. use 'none' to suppress the log file.",
+    )
+    parser.add_argument(
+        "-q",
+        action = "store_true",
+        help = "quiet flag to stop command line output, only print to log file",
+    )
+    parser.add_argument(
+        "--dtype",
+        type = str,
+        choices = ["float64", "float32"],
+        metavar = "datatype",
+        help = "set the float point precision. Must be one of: float64, float32",
+    )
+    parser.add_argument(
+        "--device",
+        type = str,
+        choices = ["cpu", "gpu"],
+        metavar = "device",
+        help = "set the device for AutoProf to use for computations. Must be one of: cpu, gpu",
+    )
+    
     args = parser.parse_args()
     
+    if args.log is not None:
+        AP_config.set_logging_output(stdout = not args.q, filename = None if args.log == "none" else args.log)
+    elif args.q:
+        AP_config.set_logging_output(stdout = not args.q, filename = "AutoProf.log")
+
+    if args.dtype is not None:
+        AP_config.dtype = torch.float64 if args.dtype == "float64" else torch.float32
+    if args.device is not None:
+        AP_config.device = "cpu" if args.device == "cpu" else "cuda:0"
+        
     if args.filename in ["tutorial", "tutorials"]:
         import requests
         tutorials = [
