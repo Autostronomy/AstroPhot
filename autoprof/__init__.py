50e19e2b12a3de6091e1e5f7d300fa77d822dbdb
Connor Stone <connorstone628@gmail.com>
2023-02-07 20:23:48 -0500

    updating version number so it will increment at next push

--- autoprof/__init__.py
+++ autoprof/__init__.py
@@ -3,7 +3,7 @@ import argparse
 from . import models, image, plots, utils, fit, AP_config
 
 # meta data
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __author__ = "Connor Stone"
 __email__ = "connorston628@gmail.com"
 
