391931e12261193b4a65614c9343f79ff84b9f47
Connor Stone <connorstone628@gmail.com>
2023-02-15 23:45:33 -0500

    adding LM unittest

--- tests/test_fit.py
+++ tests/test_fit.py
@@ -2,10 +2,14 @@ import unittest
 import autoprof as ap
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 
 class TestComponentModelFits(unittest.TestCase):
 
-    def test_sersic_fit(self):
+    def test_sersic_fit_grad(self):
+        """
+        Simply test that the gradient optimizer changes the parameters
+        """
         np.random.seed(12345)
         N = 50
         Width = 20
@@ -46,8 +50,55 @@ class TestComponentModelFits(unittest.TestCase):
         for p in mod.parameters:
             self.assertFalse(np.any(mod[p].representation.detach().numpy() == mod_initparams[p]), f"parameter {p} should update with optimization")
 
+    def test_sersic_fit_lm(self):
+        """
+        Test sersic fitting with entirely independent sersic sampling at 10x resolution.
+        """
+        np.random.seed(12345)
+        N = 50
+        pixelscale = 0.8
+        upsample = 10
+        Width = 20
+        shape = (N+10,N)
+        #true_params = [2,5,10,-3, 5, 0.7, np.pi/4]
+        true_params = {"n": 2, "Re": 10, "Ie": 1, "center": [-3.3, 5.3], "q": 0.7, "pa": np.pi/4}
+        IXX, IYY = np.meshgrid(
+            np.linspace(-Width + (pixelscale / (2*upsample)), Width - (pixelscale / (2*upsample)), shape[1]*upsample),
+            np.linspace(-Width + (pixelscale / (2*upsample)), Width - (pixelscale / (2*upsample)), shape[0]*upsample),
+        )
+        QPAXX, QPAYY = ap.utils.conversions.coordinates.Axis_Ratio_Cartesian_np(true_params["q"], IXX - true_params["center"][0], IYY - true_params["center"][1], -true_params["pa"])
+        Z0 = ap.utils.parametric_profiles.sersic_np(np.sqrt(QPAXX**2 + QPAYY**2), true_params["n"], true_params["Re"], true_params["Ie"]) + np.random.normal(loc = 0, scale = 0.1, size = (shape[0]*upsample, shape[1]*upsample))
+        Z0 = Z0.reshape(shape[0], upsample, shape[1], upsample).sum(axis = (1,3)) / upsample**2
+        tar = ap.image.Target_Image(
+            data = Z0,
+            pixelscale = pixelscale,
+            variance = np.ones(Z0.shape)*(0.1**2),
+        )
+
+        mod = ap.models.Sersic_Galaxy(
+            name = "sersic model",
+            target = tar,
+        )
+        
+        mod.initialize()
+
+        ap.AP_config.set_logging_output(stdout= True,filename = "AutoProf.log")
+        res = ap.fit.LM(model = mod, verbose = 1).fit()
+        
+        self.assertAlmostEqual(mod["center"].value[0].item() / (true_params["center"][0] + shape[1]*pixelscale/2), 1, 1, "LM should accurately recover parameters in simple cases")
+        self.assertAlmostEqual(mod["center"].value[1].item() / (true_params["center"][1] + shape[0]*pixelscale/2), 1, 1, "LM should accurately recover parameters in simple cases")
+        self.assertAlmostEqual(mod["n"].value.item(), true_params["n"], 2, msg = "LM should accurately recover parameters in simple cases")
+        self.assertAlmostEqual(mod["Re"].value.item() / true_params["Re"], 1, delta = 1, msg = "LM should accurately recover parameters in simple cases")
+        self.assertAlmostEqual(mod["Ie"].value.item(), np.log10(true_params["Ie"]/pixelscale**2), 3, "LM should accurately recover parameters in simple cases")
+        self.assertAlmostEqual(mod["PA"].value.item() / true_params["pa"], 1, delta = 0.5, msg = "LM should accurately recover parameters in simple cases")
+        self.assertAlmostEqual(mod["q"].value.item(), true_params["q"], 1, "LM should accurately recover parameters in simple cases")
+
+            
 class TestGroupModelFits(unittest.TestCase):
     def test_groupmodel_fit(self):
+        """
+        Simply test that fitting a group model changes the parameter values
+        """
         np.random.seed(12345)
         N = 50
         Width = 20
