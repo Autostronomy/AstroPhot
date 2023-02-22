85b2c30c3cf7aac75bb4f09c21e75832d59ebefb
Ranbir <71406920+ranbir7@users.noreply.github.com>
2023-02-23 00:47:50 +0530

    Update image_object.py

--- autoprof/image/image_object.py
+++ autoprof/image/image_object.py
@@ -33,8 +33,8 @@ class BaseImage(object):
             filename: Optional[str] = None, 
             zeropoint: Optional[Union[float, torch.Tensor]] = None, 
             note: Optional[str] = None, 
-            origin: center : Optional[Sequence] = None, 
-            center: center : Optional[Sequence] = None, 
+            origin: Optional[Sequence] = None, 
+            center: Optional[Sequence] = None, 
             **kwargs: Any) -> None:
 
         """Initialize an instance of the APImage class.
