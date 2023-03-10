1c9651663b2727244e33fff0cd74d746ffc24937
Ranbir <ranbirdas176@gmail.com>
2023-03-10 17:27:50 +0000

    added doc string to _model_methods_.py file.

--- autoprof/models/_model_methods.py
+++ autoprof/models/_model_methods.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from typing import Optional, Union, Dict, Tuple, Any
 from copy import deepcopy
 from .parameter_object import Parameter
 from ..utils.conversions.coordinates import coord_to_index, index_to_coord
@@ -7,25 +8,52 @@ from ..image import Model_Image, Target_Image, Window
 from .. import AP_config
 
 
-def integrate_window(self, image, fix_to = "center"):
-    """The appropriately sized window in which to perform integration for
+def integrate_window(self, image: "Model_Image", fix_to: str() = "center") -> Window:
+    """
+    The appropriately sized window in which to perform integration for
     this model, centered on the model center.
+    Args:
+    image (Model_Image): The image object to integrate.
+
+    fix_to (str): Specifies whether to fix the window to the model center or the nearest pixel center.
+                  Should be one of: center, pixel. Defaults to "center".
+
+    Returns: Window which is The window object to integrate.
+
+    Raises: ValueError -> If the value of fix_to is not valid.
 
     """
     if fix_to == "center":
         use_center = self["center"].value
     elif fix_to == "pixel":
         align = image.pixel_center_alignment()
-        use_center = (align + torch.round(self["center"].value/image.pixelscale - align))*image.pixelscale
+        use_center = (
+            align + torch.round(self["center"].value / image.pixelscale - align)
+        ) * image.pixelscale
     else:
-        raise ValueError(f"integrate_window fix_to should be one of: center, pixel. not {fix_to}")
-    window_align = torch.isclose(((use_center - image.origin)/image.pixelscale) % 1, torch.tensor(0.5, dtype = AP_config.ap_dtype, device = AP_config.ap_device), atol = 0.25)
-    request_pixels = (self.integrate_window_size * self.target.pixelscale / image.pixelscale).to(dtype = torch.int32)
-    use_shape = ((request_pixels + 1 - (request_pixels % 2) + 1 - window_align.to(dtype = torch.int32))*image.pixelscale)
-    return Window(center = use_center, shape = use_shape)
+        raise ValueError(
+            f"integrate_window fix_to should be one of: center, pixel. not {fix_to}"
+        )
+    window_align = torch.isclose(
+        ((use_center - image.origin) / image.pixelscale) % 1,
+        torch.tensor(0.5, dtype=AP_config.ap_dtype, device=AP_config.ap_device),
+        atol=0.25,
+    )
+    request_pixels = (
+        self.integrate_window_size * self.target.pixelscale / image.pixelscale
+    ).to(dtype=torch.int32)
+    use_shape = (
+        request_pixels
+        + 1
+        - (request_pixels % 2)
+        + 1
+        - window_align.to(dtype=torch.int32)
+    ) * image.pixelscale
+    return Window(center=use_center, shape=use_shape)
+
 
 @classmethod
-def build_parameter_specs(cls, user_specs = None):
+def build_parameter_specs(cls, user_specs=None):
     parameter_specs = {}
     for base in cls.__bases__:
         try:
@@ -39,12 +67,15 @@ def build_parameter_specs(cls, user_specs = None):
             # If the user supplied a parameter object subclass, simply use that as is
             if isinstance(user_specs[p], Parameter):
                 parameter_specs[p] = user_specs[p]
-            elif isinstance(user_specs[p], dict): # if the user supplied parameter specifications, update the defaults
+            elif isinstance(
+                user_specs[p], dict
+            ):  # if the user supplied parameter specifications, update the defaults
                 parameter_specs[p].update(user_specs[p])
             else:
                 parameter_specs[p]["value"] = user_specs[p]
-                
+
     return parameter_specs
+
 
 def build_parameters(self):
     for p in self.parameter_specs:
@@ -62,6 +93,7 @@ def build_parameters(self):
         else:
             raise ValueError(f"unrecognized parameter specification for {p}")
 
+
 def __str__(self):
     state = self.get_state()
     presentation = ""
@@ -69,20 +101,22 @@ def __str__(self):
         presentation = presentation + f"{key}: {state[key]}\n"
     return presentation
 
+
 def __getitem__(self, key):
     # Access an element from an array parameter
     if isinstance(key, tuple):
         return self.parameters[key[0]][key[1]]
-        
+
     # Try to access the parameter by name
     if key in self.parameters:
         return self.parameters[key]
 
     # Try to get a particular element from an array parameter
-    if "|" in key and key[:key.find("|")] in self.parameters:
-        return self.parameters[key[:key.find("|")]][int(key[key.find("|")+1:])]
-        
+    if "|" in key and key[: key.find("|")] in self.parameters:
+        return self.parameters[key[: key.find("|")]][int(key[key.find("|") + 1 :])]
+
     raise KeyError(f"{key} not in {self.name}. {str(self)}")
+
 
 def __contains__(self, key):
     try:
