957556d96a623b31c2b6814c8586f38eea2659d8
Ranbir <71406920+ranbir7@users.noreply.github.com>
2023-03-12 21:14:40 +0530

    Update __init__.py

--- autoprof/plots/__init__.py
+++ autoprof/plots/__init__.py
@@ -1,9 +1,4 @@
-# The Profile module provides a class for reading and parsing profile data
 from .profile import Profile
-
-# The Image module provides a class for reading and processing image data
 from .image import Image
-
-# The Visuals module provides functions for creating visualizations of data
 from .visuals import create_visualization
 
