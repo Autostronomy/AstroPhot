38353d429ad52b4fd85d63b9cc3883a43513c603
Connor Stone <connorstone628@gmail.com>
2023-05-11 11:01:40 -0400

    ensure tensor is on correct device/dtype for parameter boundaries

--- autoprof/utils/conversions/optimization.py
+++ autoprof/utils/conversions/optimization.py
@@ -1,10 +1,11 @@
 import numpy as np
 import torch
+from ... import AP_config
 
 
 def boundaries(val, limits):
     """val in limits expanded to range -inf to inf"""
-    tval = val if isinstance(val, torch.Tensor) else torch.tensor(val)
+    tval = val if isinstance(val, torch.Tensor) else torch.tensor(val,device=AP_config.ap_device, dtype = AP_config.ap_dtype)
     if limits[0] is None:
         return tval - 1.0 / (tval - limits[1])  # fixme check + or -?
     elif limits[1] is None:
@@ -15,7 +16,7 @@ def boundaries(val, limits):
 def inv_boundaries(val, limits):
     """val in range -inf to inf compressed to within the limits"""
 
-    tval = torch.as_tensor(val)
+    tval = torch.as_tensor(val, device = AP_config.ap_device, dtype = AP_config.ap_dtype)
     if limits[0] is None:
         return (tval + limits[1] - torch.sqrt(torch.pow(tval - limits[1], 2) + 4)) * 0.5
     elif limits[1] is None:
@@ -27,7 +28,7 @@ def inv_boundaries(val, limits):
 
 def d_boundaries_dval(val, limits):
     """derivative of: val in limits expanded to range -inf to inf"""
-    tval = torch.as_tensor(val)
+    tval = torch.as_tensor(val, device = AP_config.ap_device, dtype = AP_config.ap_dtype)
     if limits[0] is None:
         return 1.0 + 1.0 / (tval - limits[1]) ** 2
     elif limits[1] is None:
@@ -39,7 +40,7 @@ def d_boundaries_dval(val, limits):
 
 def d_inv_boundaries_dval(val, limits):
     """derivative of: val in range -inf to inf compressed to within the limits"""
-    tval = torch.as_tensor(val)
+    tval = torch.as_tensor(val, device = AP_config.ap_device, dtype = AP_config.ap_dtype)
     if limits[0] is None:
         return 0.5 - 0.5 * (tval - limits[1]) / torch.sqrt(
             torch.pow(tval - limits[1], 2) + 4
@@ -53,7 +54,7 @@ def d_inv_boundaries_dval(val, limits):
 
 def cyclic_boundaries(val, limits):
     """Applies cyclic boundary conditions to the input value."""
-    tval = val if isinstance(val, torch.Tensor) else torch.tensor(val)
+    tval = val if isinstance(val, torch.Tensor) else torch.tensor(val,device=AP_config.ap_device, dtype = AP_config.ap_dtype)
     return limits[0] + ((tval - limits[0]) % (limits[1] - limits[0]))
 
 
@@ -62,8 +63,8 @@ def cyclic_difference_torch(val1, val2, period):
     boundary conditions.
 
     """
-    tval1 = val1 if isinstance(val1, torch.Tensor) else torch.tensor(val1)
-    tval2 = val2 if isinstance(val2, torch.Tensor) else torch.tensor(val2)
+    tval1 = val1 if isinstance(val1, torch.Tensor) else torch.tensor(val1, device = AP_config.ap_device, dtype = AP_config.ap_dtype)
+    tval2 = val2 if isinstance(val2, torch.Tensor) else torch.tensor(val2, device = AP_config.ap_device, dtype = AP_config.ap_dtype)
     return torch.arcsin(torch.sin((tval1 - tval2) * np.pi / period)) * period / np.pi
 
 
