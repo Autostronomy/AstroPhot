fix pixel shift units

autophot/models/_model_methods.py
@@ -157,16 +157,21 @@ def _sample_integrate(self, deep, reference, image, parameters, center):
 
 
 def _sample_convolve(self, image, shift, psf):
+    """
+    image: Image object with image.data pixel matrix
+    shift: the amount of shifting to do in pixel units
+    psf: a PSF_Image object
+    """
+    
     if shift is not None:
         if any(np.array(psf.data.shape) < 10):
             psf_data = torch.nn.functional.pad(psf.data, (2, 2, 2, 2))
         else:
             psf_data = psf.data
-        pix_center_shift = image.world_to_pixel_delta(shift)
         LL = _shift_Lanczos_kernel_torch(
-            -pix_center_shift[0],
-            -pix_center_shift[1],
-            2,
+            -shift[0],
+            -shift[1],
+            3,
             AP_config.ap_dtype,
             AP_config.ap_device,
         )

autophot/models/model_object.py
@@ -322,7 +322,7 @@ class Component_Model(AutoPhot_Model):
                 pixel_center = working_image.world_to_pixel(parameters["center"].value)
                 center_shift = pixel_center - torch.round(
                     pixel_center
-                )  # torch.clamp(pixel_center - torch.round(pixel_center), -0.49, 0.49) # shifts smaller than 1/100th of a pixel are not allowed for numerical stability
+                )
                 working_image.header.pixel_shift_origin(center_shift)
             else:
                 center_shift = None
@@ -345,7 +345,7 @@ class Component_Model(AutoPhot_Model):
 
             # Shift image back to align with original pixel grid
             if self.psf_subpixel_shift:
-                working_image.header.shift_origin(-center_shift)
+                working_image.header.pixel_shift_origin(-center_shift)
             # Add the sampled/integrated/convolved pixels to the requested image
             working_image = working_image.reduce(psf.psf_upscale).crop(
                 psf.psf_border_int