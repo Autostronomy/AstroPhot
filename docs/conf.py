ed1d35901643af23a9879f237e969e2a465c33af
Connor Stone <connorstone628@gmail.com>
2023-06-19 11:07:20 -0400

    build for version 0.10.2

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = "Connor Stone"
 
 # Version information
 version = "0.10"
-release = "0.10.1"
+release = "0.10.2"
 
 # Patterns of files and directories to exclude
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
