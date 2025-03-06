536486ee7eaf7f30f6fd0b5a81458e4d72057a9f
Connor Stone <connorstone628@gmail.com>
2025-03-05 22:17:52 -0500

    demo psf group model

--- astrophot/models/group_psf_model.py
+++ astrophot/models/group_psf_model.py
@@ -1,5 +1,7 @@
+from typing import Optional
+
 from .group_model_object import Group_Model
-from ..image import PSF_Image
+from ..image import PSF_Image, Image, Window
 from ..errors import InvalidTarget
 
 __all__ = ["PSF_Group_Model"]
@@ -9,6 +11,7 @@ class PSF_Group_Model(Group_Model):
 
     model_type = f"psf {Group_Model.model_type}"
     usable = True
+    normalize_psf = True
 
     @property
     def psf_mode(self):
@@ -34,3 +37,14 @@ class PSF_Group_Model(Group_Model):
         if hasattr(self, "models"):
             for model in self.models.values():
                 model.target = tar
+
+    # def sample(
+    #     self,
+    #     image: Optional[Image] = None,
+    #     window: Optional[Window] = None,
+    #     parameters: Optional["Parameter_Node"] = None,
+    # ):
+    #     image = super().sample(image, window, parameters)
+    #     if self.normalize_psf:
+    #         image.data /= image.data.sum()
+    #     return image
