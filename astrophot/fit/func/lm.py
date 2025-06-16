870af0fd4d7a23676f9dd92c61f43fd872e359a2
Connor Stone <connorstone628@gmail.com>
2025-06-16 09:38:13 -0400

    starting on LM

--- astrophot/fit/func/lm.py
+++ astrophot/fit/func/lm.py
@@ -10,43 +10,66 @@ def gradient(J, W, R):
     return -J.T @ (W * R)
 
 
-def step(L, grad, hess):
-    I = torch.eye(len(grad), dtype=grad.dtype, device=grad.device)
+def damp_hessian(hess, L):
+    I = torch.eye(len(hess), dtype=hess.dtype, device=hess.device)
     D = torch.ones_like(hess) - I
-
-    h = torch.linalg.solve(
-        hess * (I + D / (1 + L)) + L * I * (1 + torch.diag(hess)),
-        grad,
-    )
-
-    return h
+    return hess * (I + D / (1 + L)) + L * I * (1 + torch.diag(hess))
 
 
 def step(x, data, model, weight, jacobian, ndf, chi2, L=1.0, Lup=9.0, Ldn=10.0):
 
+    chi20 = chi2
     M0 = model(x)
     J = jacobian(x)
     R = data - M0
     grad = gradient(J, weight, R)
     hess = hessian(J, weight)
 
-    best = {"h": torch.zeros_like(x), "chi2": chi2, "L": L}
-    scary = {"h": None, "chi2": chi2, "L": L}
+    best = {"h": torch.zeros_like(x), "chi2": chi20, "L": L}
+    scary = {"h": None, "chi2": chi20, "L": L}
 
+    nostep = True
     improving = None
     for i in range(10):
-        h = step(L, grad, hess)
+        hessD = damp_hessian(hess, L)
+        h = torch.linalg.solve(hessD, grad)
         M1 = model(x + h)
 
-        chi2 = torch.sum(weight * (data - M1) ** 2).item() / ndf
+        chi21 = torch.sum(weight * (data - M1) ** 2).item() / ndf
 
         # Handle nan chi2
-        if not np.isfinite(chi2):
+        if not np.isfinite(chi21):
             L *= Lup
             if improving is True:
                 break
             improving = False
             continue
 
-        if chi2 < scary["chi2"]:
-            scary = {"h": h, "chi2": chi2, "L": L}
+        if chi21 < scary["chi2"]:
+            scary = {"h": h, "chi2": chi21, "L": L}
+
+        rho = (chi20 - chi21) / torch.abs(h.T @ hessD @ h - 2 * grad @ h)
+
+        # Larger higher order terms
+        if rho < 0.1:
+            L *= Lup
+            if improving is True:
+                break
+            improving = False
+            continue
+
+        if chi21 < best["chi2"]:  # new best
+            best = {"h": h, "chi2": chi21, "L": L}
+            improving = True
+            nostep = False
+            L /= Ldn
+            if L < 1e-8 or improving is False:
+                break
+            improving = True
+        elif improving is True:
+            break
+        else:  # not improving and bad chi2, damp more
+            L *= Lup
+            if L >= 1e9:
+                break
+            improving = False
