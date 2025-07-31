cfa8c40a097d1ecc302e6dfe56ff1971fddd0476
Connor Stone <connorstone628@gmail.com>
2025-07-31 10:53:43 -0400

    fix utils docs

--- astrophot/utils/parametric_profiles.py
+++ astrophot/utils/parametric_profiles.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import ndarray
 from .conversions.functions import sersic_n_to_b
 
 __all__ = (
@@ -12,17 +13,17 @@ __all__ = (
 )
 
 
-def sersic_np(R, n, Re, Ie):
+def sersic_np(R: ndarray, n: ndarray, Re: ndarray, Ie: ndarray) -> ndarray:
     """Sersic 1d profile function, works more generally with numpy
     operations. In the event that impossible values are passed to the
     function it returns large values to guide optimizers away from
     such values.
 
-    Parameters:
-        R: Radii array at which to evaluate the sersic function
-        n: sersic index restricted to n > 0.36
-        Re: Effective radius in the same units as R
-        Ie: Effective surface density
+    **Args:**
+    -  `R`: Radii array at which to evaluate the sersic function
+    -  `n`: sersic index restricted to n > 0.36
+    -  `Re`: Effective radius in the same units as R
+    -  `Ie`: Effective surface density
     """
     if np.any(np.array([n, Re, Ie]) <= 0):
         return np.ones(len(R)) * 1e6
@@ -30,53 +31,54 @@ def sersic_np(R, n, Re, Ie):
     return Ie * np.exp(-bn * ((R / Re) ** (1 / n) - 1))
 
 
-def gaussian_np(R, sigma, I0):
+def gaussian_np(R: ndarray, sigma: ndarray, I0: ndarray) -> ndarray:
     """Gaussian 1d profile function, works more generally with numpy
     operations.
 
-    Parameters:
-        R: Radii array at which to evaluate the sersic function
-        sigma: standard deviation of the gaussian in the same units as R
-        I0: central surface density
+    **Args:**
+    -  `R`: Radii array at which to evaluate the gaussian function
+    -  `sigma`: standard deviation of the gaussian in the same units as R
+    -  `I0`: central surface density
     """
     return (I0 / np.sqrt(2 * np.pi * sigma**2)) * np.exp(-0.5 * ((R / sigma) ** 2))
 
 
-def exponential_np(R, Ie, Re):
+def exponential_np(R: ndarray, Ie: ndarray, Re: ndarray) -> ndarray:
     """Exponential 1d profile function, works more generally with numpy
     operations.
 
-    Parameters:
-        R: Radii array at which to evaluate the sersic function
-        Re: Effective radius in the same units as R
-        Ie: Effective surface density
+    **Args:**
+    -  `R`: Radii array at which to evaluate the exponential function
+    -  `Ie`: Effective surface density
+    -  `Re`: Effective radius in the same units as R
     """
     return Ie * np.exp(-sersic_n_to_b(1.0) * (R / Re - 1.0))
 
 
-def moffat_np(R, n, Rd, I0):
+def moffat_np(R: ndarray, n: ndarray, Rd: ndarray, I0: ndarray) -> ndarray:
     """Moffat 1d profile function, works with numpy operations.
 
-    Parameters:
-        R: Radii tensor at which to evaluate the moffat function
-        n: concentration index
-        Rd: scale length in the same units as R
-        I0: central surface density
-
+    **Args:**
+    -  `R`: Radii array at which to evaluate the moffat function
+    -  `n`: concentration index
+    -  `Rd`: scale length in the same units as R
+    -  `I0`: central surface density
     """
     return I0 / (1 + (R / Rd) ** 2) ** n
 
 
-def nuker_np(R, Rb, Ib, alpha, beta, gamma):
+def nuker_np(
+    R: ndarray, Rb: ndarray, Ib: ndarray, alpha: ndarray, beta: ndarray, gamma: ndarray
+) -> ndarray:
     """Nuker 1d profile function, works with numpy functions
 
-    Parameters:
-        R: Radii tensor at which to evaluate the nuker function
-        Ib: brightness at the scale length, represented as the log of the brightness divided by pixel scale squared.
-        Rb: scale length radius
-        alpha: sharpness of transition between power law slopes
-        beta: outer power law slope
-        gamma: inner power law slope
+    **Args:**
+    -  `R`: Radii tensor at which to evaluate the nuker function
+    -  `Ib`: brightness at the scale length, represented as the log of the brightness divided by pixel scale squared.
+    -  `Rb`: scale length radius
+    -  `alpha`: sharpness of transition between power law slopes
+    -  `beta`: outer power law slope
+    -  `gamma`: inner power law slope
 
     """
     return (
@@ -87,52 +89,30 @@ def nuker_np(R, Rb, Ib, alpha, beta, gamma):
     )
 
 
-def ferrer_np(R, rout, alpha, beta, I0):
+def ferrer_np(R: ndarray, rout: ndarray, alpha: ndarray, beta: ndarray, I0: ndarray) -> ndarray:
     """
     Modified Ferrer profile.
 
-    Parameters
-    ----------
-    R : array_like
-        Radial distance from the center.
-    rout : float
-        Outer radius of the profile.
-    alpha : float
-        Power-law index.
-    beta : float
-        Exponent for the modified Ferrer function.
-    I0 : float
-        Central intensity.
-
-    Returns
-    -------
-    array_like
-        The modified Ferrer profile evaluated at R.
+    **Args:**
+    -  `R`: Radial distance from the center.
+    -  `rout`: Outer radius of the profile.
+    -  `alpha`: Power-law index.
+    -  `beta`: Exponent for the modified Ferrer function.
+    -  `I0`: Central intensity.
     """
     return (R < rout) * I0 * ((1 - (np.clip(R, 0, rout) / rout) ** (2 - beta)) ** alpha)
 
 
-def king_np(R, Rc, Rt, alpha, I0):
+def king_np(R: ndarray, Rc: ndarray, Rt: ndarray, alpha: ndarray, I0: ndarray) -> ndarray:
     """
     Empirical King profile.
 
-    Parameters
-    ----------
-    R : array_like
-        The radial distance from the center.
-    Rc : float
-        The core radius of the profile.
-    Rt : float
-        The truncation radius of the profile.
-    alpha : float
-        The power-law index of the profile.
-    I0 : float
-        The central intensity of the profile.
-
-    Returns
-    -------
-    array_like
-        The intensity at each radial distance.
+    **Args:**
+    -  `R`: The radial distance from the center.
+    -  `Rc`: The core radius of the profile.
+    -  `Rt`: The truncation radius of the profile.
+    -  `alpha`: The power-law index of the profile.
+    -  `I0`: The central intensity of the profile.
     """
     beta = 1 / (1 + (Rt / Rc) ** 2) ** (1 / alpha)
     gamma = 1 / (1 + (R / Rc) ** 2) ** (1 / alpha)
