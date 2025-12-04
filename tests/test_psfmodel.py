5c75c8fb162559c2d71a999eec7c332d75c5a1a3
Connor Stone <connorstone628@gmail.com>
2025-12-04 16:25:23 -0500

    ensure dynamic for some params

--- tests/test_psfmodel.py
+++ tests/test_psfmodel.py
@@ -17,11 +17,11 @@ def test_all_psfmodel_sample(model_type):
         )
 
     if "nuker" in model_type:
-        kwargs = {"Ib": None}
+        kwargs = {"Ib": {"value": None, "dynamic": True}}
     elif "gaussian" in model_type:
-        kwargs = {"flux": None}
+        kwargs = {"flux": {"value": None, "dynamic": True}}
     elif "exponential" in model_type:
-        kwargs = {"Ie": None}
+        kwargs = {"Ie": {"value": None, "dynamic": True}}
     else:
         kwargs = {}
     target = make_basic_gaussian_psf(pixelscale=0.8)
