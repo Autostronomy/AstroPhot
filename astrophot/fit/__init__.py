b9f54a94eb06b8d0c8610618690b848629691325
Connor Stone <connorstone628@gmail.com>
2026-06-24 13:21:21 -0400

    remove grad reference

--- astrophot/fit/__init__.py
+++ astrophot/fit/__init__.py
@@ -1,6 +1,6 @@
 from .lm import LM, LMConstraint
 from .batch_lm import BatchLM
-from .gradient import Grad, Slalom
+from .gradient import Slalom
 from .iterative import Iter, IterParam
 from .scipy_fit import ScipyFit
 from .hmc import HMC
@@ -12,7 +12,6 @@ __all__ = [
     "LM",
     "LMConstraint",
     "BatchLM",
-    "Grad",
     "Iter",
     "MALA",
     "IterParam",
